68d7df494c7e44ad9e7c9adc4a2ef48436d9e005
yujinhor <puppyday01@konkuk.ac.kr>
2024-11-07 19:05:12 +0900

    NavMesh_Crosswalk
    아직 수정 필요

--- Assets/Scripts/People/npcController.cs
+++ Assets/Scripts/People/npcController.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class npcController : MonoBehaviour {
-
+public class npcController : MonoBehaviour
+{
     public NavMeshAgent agent;
     public Animator animator;
 
-    public GameObject PATH;
+    public GameObject PATH; // Pavewalk
     public Transform[] PathPoints;
 
     public float minDistance = 10;
@@ -16,32 +16,31 @@ public class npcController : MonoBehaviour {
     public int index = 0;
 
 
-    void Start() {
+    void Start()
+    {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
 
+        // PATH에서 모든 자식 포인트 가져오기
         PathPoints = new Transform[PATH.transform.childCount];
-        for (int i = 0; i < PathPoints.Length; i++) {
+        for (int i = 0; i < PathPoints.Length; i++)
+        {
             PathPoints[i] = PATH.transform.GetChild(i);
         }
     }
 
-    private void Update() {
+    void Update() {
         roam();
     }
 
-    void roam()
-    {
-        if (Vector3.Distance(transform.position, PathPoints[index].position) < minDistance) {
-            if (index >= 0 && index < PathPoints.Length) {
-                index += 1;
-            }
-            else {
-                index = 0;
-            }
+    void roam() {
+        // 다음 PathPoint로 이동
+        if (Vector3.Distance(transform.position, PathPoints[index].position) < minDistance)
+        {
+            index = (index + 1) % PathPoints.Length; // 순환 구조로 이동
         }
 
         agent.SetDestination(PathPoints[index].position);
-        animator.SetFloat("vertical", !agent.isStopped ? 1 : 0);
+        animator.SetFloat("vertical", !agent.isStopped ? 1 : 0); // 애니메이션 동기화
     }
 }
