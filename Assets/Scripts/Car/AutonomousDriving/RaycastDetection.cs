44d1ce8970b45587b96ebce6be13fc3efb1dbe90
Growcompany <66565360+Growcompany@users.noreply.github.com>
2024-11-29 20:48:33 +0900

    Navi4&TrafficLIght&RayCast&PeopleColiisionRigidbody
    .Navi4&TrafficLIght&RayCast&PeopleColiisionRigidbody

--- Assets/Scripts/Car/AutonomousDriving/RaycastDetection.cs
+++ Assets/Scripts/Car/AutonomousDriving/RaycastDetection.cs
@@ -5,10 +5,12 @@ public class RaycastDetection : MonoBehaviour
 {
     private VPStandardInput vpInput;
 
-    public float rayLength = 3f; // 레이의 길이
+    public float rayLength = 5f; // 레이의 길이
     public LayerMask detectionLayer; // 감지할 레이어 설정
-    public float rayHeightOffset = 0f; // Y축 높이
+    public float rayHeightOffset = 0.5f; // Y축 높이
     public float rayBackwardOffset = 3f; // 뒤쪽으로 이동할 거리
+    private Vector3 rayOrigin; // 디버그용 레이 시작점
+    private Vector3 rayDirection; // 디버그용 레이 방향
 
     private void Start()
     {
@@ -27,16 +29,23 @@ public class RaycastDetection : MonoBehaviour
         // 차량 속도 확인
         float vehicleSpeed = vpInput.vehicle.speed;
 
+        rayLength = (float)(3 + (0.5*vehicleSpeed));
+        rayBackwardOffset = -(float)(0.5 * vehicleSpeed);
+
+        // 레이 시작 위치와 방향 설정
+        rayOrigin = transform.position + Vector3.up * rayHeightOffset - transform.forward * rayBackwardOffset; // 뒤쪽으로 오프셋 추가
+        rayDirection = transform.forward;
+
+        // 디버그용 레이 시각화
+        Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.red);
+
+
         if (vehicleSpeed < 1)
         {
             vpInput.externalHandbrake = 0f;
             vpInput.externalBrake = 0f;
             return;
         }
-
-        // 레이 시작 위치와 방향 설정
-        Vector3 rayOrigin = transform.position + Vector3.up * rayHeightOffset - transform.forward * rayBackwardOffset; // 뒤쪽으로 오프셋 추가
-        Vector3 rayDirection = transform.forward;
 
         // 레이캐스트 히트 정보 저장
         RaycastHit hitInfo;
@@ -61,8 +70,16 @@ public class RaycastDetection : MonoBehaviour
             Debug.Log("No objects detected in front.");
         }
 
-        // 디버그용 레이 시각화
-        Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.red);
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        // 레이의 색상을 설정
+        Gizmos.color = Color.red;
+
+        // 레이를 시각적으로 표시
+        Gizmos.DrawLine(rayOrigin, rayOrigin + rayDirection * rayLength);
     }
 
 }
